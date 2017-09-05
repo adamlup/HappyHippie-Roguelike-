c1939091c9e0c86ba1f4bb5b9cc0a9e7e7e37d6f
adamlup <adamlupi@gmail.com>
2017-09-05 10:44:15 +0200

    level one board

--- levels_board.py
+++ levels_board.py
@@ -21,20 +21,3 @@ def level_one_board():
     ##############################################################
     '''
     return level_one_board
-'''
-def create_board(level_one_board):
-    level_one_board = level_one_board.split("\n")
-    board = []
-    for elm in level_one_board:
-        board.append(elm)
-    
-    return board
-'''
-def insert_player(board, width, height):
-    board[height][width] = '@'
-    return board
-'''
-def print_board(board):
-    for row in board:
-        print(row, end='')
-'''        
