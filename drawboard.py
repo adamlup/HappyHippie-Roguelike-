89216a57e27ad1b3a30433fd3dad79e4bb290843
adamlup <adamlupi@gmail.com>
2017-09-05 09:42:29 +0200

    printing board

---
+++ drawboard.py
@@ -0,0 +1,18 @@
+import levels_board
+
+def draw_board():
+    level_one_board = levels_board.level_one_board()
+    return level_one_board
+
+def create_board(level_one_board):
+    level_one_board = level_one_board.split("\n")
+    board = []
+
+    for element in level_one_board:
+        board.append(list(element))
+    return board
+
+def print_board(board):
+    for row in board:
+        for char in row:
+            print(char, end='')
