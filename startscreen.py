fd6ef883c527b7663dd38975cb1541f1ce5668d1
adamlup <adamlupi@gmail.com>
2017-09-05 09:19:11 +0200

    beginning screen

--- startscreen.py
+++ startscreen.py
@@ -1,13 +1,16 @@
-CITY = (
+import time
+import os
+
+start = (
 """
 '||  ||`
- ||  ||                           ''
- ||''||   '''|.  '||''|, '||''|,  ||  .|''|,
- ||  ||  .|''||   ||  ||  ||  ||  ||  ||..||
-.||  ||. `|..||.  ||..|'  ||..|' .||. `|...
-                  ||      ||
-                 .||     .||
-'||  ||`
+ ||  ||                           
+ ||''||   '''|.  '||''|, '||''|,  ||   //
+ ||  ||  .|''||   ||  ||  ||  ||  ||  //
+.||  ||. `|..||.  ||..|'  ||..|'   \,//
+                  ||      ||        //
+                 .||     .||       //
+'||  ||`                          
  ||  ||   ''                   ''
  ||''||   ||  '||''|, '||''|,  ||  .|''|,
  ||  ||   ||   ||  ||  ||  ||  ||  ||..||
@@ -33,4 +36,49 @@ ___________________________________________________________________________
 ___________________________________________________________________________
 """)
 
-print (CITY)
+story = (
+'''
+###########################################################################
+#                                                                         #
+#                                                                         #
+#This is a simple story about Happy Hippie. Well...he was happy years ago.#
+#Now he ​misses​ his past ​life. He has to wear a truly uncomfortable suit   #
+#and shiny shoes. You know ​that ​this is not the way of ​life ​that works ​for#
+#him so help him to get out of there before he loses all the joy of life. #
+#If that happens, he will become a low paid corpo-worker forever.         #
+#                                                                         #
+#                                                                         #
+###########################################################################
+''')
+
+autors = (
+'''
+###########################################################################
+#                                                                         #
+#                          Happy Hippie                                   #
+#                                                                         #
+#                    The game was created by:                             #
+#                   Magdalena Kiełkowicz-Werner                           #
+#                               &                                         #
+#                         Adam Łupikasza                                  #
+#                                                                         #
+#                                                                         #
+###########################################################################
+'''
+)
+
+
+def print_start():
+    for letter in start:
+        print(letter, end='')
+        time.sleep(0.004)
+    time.sleep(2)
+    os.system("clear")
+    print(autors)
+    time.sleep(4)
+    os.system("clear")
+    print(story)
+    time.sleep(16)
+    os.system("clear")
+
+
