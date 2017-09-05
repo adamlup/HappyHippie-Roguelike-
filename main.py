08f06139686e57f3ecb242aae5425f2328963b94
adamlup <adamlupi@gmail.com>
2017-09-05 21:25:39 +0200

    moving without leaving '@' trace

--- main.py
+++ main.py
@@ -18,48 +18,55 @@ def main():
     hp, attack = hero_create.hero_create()
     hp, attack = stats.hero_stats(hp, attack)
     live, atc = stats.enemy_stats(30, 15)
-
+    score = 0
     while True:
-        print("\tHero life = ", hp, "\n\tHero attack = ", attack)
-        player_place = board[player_y][player_x]
+        print("\tHero life = ", hp, "\t","\t","\t","\tscore = ", score, "\n\tHero attack = ", attack, end='')
+        place_before = board[player_y][player_x]
         board = drawboard.insert_player(board, player_x, player_y)
         drawboard.print_board(board)
+
+        old_player_x = player_x
+        old_player_y = player_y
         key = getch.getch()
         os.system("clear")
         
-        old_player_x = player_x
-        old_player_y = player_y
-
         if key == 'w':
             player_y -= 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',' :
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
                 player_y += 1
 
         elif key == 'a':
             player_x -= 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~'  or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
                 player_x += 1
-            
+
+         
         elif key == 's':
             player_y += 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~'  or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
                 player_y -= 1
             
         elif key == 'd':
             player_x += 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~'  or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
                 player_x -= 1
 
         elif key == 'q':
             break
 
-        if player_place == "O":
+        if place_before == "O":
             print("Player life {} attack enemy {}".format(hp, attack))
             hp -= atc
             print("Enemy life {} attack player {}".format(live, atc))
             live -= attack
-        #if live <= 0:
-            #board[old_player_x[old_player_y]
+            if live <= 0:
+                board[old_player_y][old_player_x] = '*'
+                score += 1
+                live = 30
+            elif live > 0:
+                board[old_player_y][old_player_x] = place_before
         if hp <= 0:
             break
+        elif place_before != "O":
+            board[old_player_y][old_player_x] = place_before
 main()
