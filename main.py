292e34f0252ca28f6cd79d02c5ea6a99d27e1bba
adamlup <adamlupi@gmail.com>
2017-09-06 15:00:31 +0200

    hall of fame, next level

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import stats
 import time
 import points
 import hall_of_fame
+import coldhot
 
 def main():
     #startscreen.print_start()
@@ -19,7 +20,6 @@ def main():
     player_y = 2
     
     hp, attack = hero_create.hero_create()
-    #hp, attack = stats.hero_stats(hp, attack)
     live, atc = stats.enemy_stats(30, 15)
     score = 0
     name = input("Enter your name: ")
@@ -32,7 +32,6 @@ def main():
 
         old_player_x = player_x
         old_player_y = player_y
-    
         key = getch.getch()
         os.system("clear")
         
@@ -57,9 +56,8 @@ def main():
                 player_x -= 1
 
         elif key == 'q':
-            # points.end_game()
             end = points.end_game()
-            hall_of_fame.write_to_file(name, start,  end, score)
+            hall_of_fame.write_to_file(name, score)
             break
         
         elif place_before == "p":
@@ -67,7 +65,7 @@ def main():
             hp += 45
             place_before = " "
             
-        elif place_before == "O":
+        if place_before == "O":
             print("Player life {} attack enemy {}".format(hp, attack))
             hp -= atc
             print("Enemy life {} attack player {}".format(live, atc))
@@ -79,9 +77,18 @@ def main():
                 live = 30
             elif live > 0:
                 board[old_player_y][old_player_x] = place_before
+        
+        if score == 1 and place_before == "B":
+            level_second_board = drawboard.draw_second_board()
+            board = drawboard.create_second_board(level_second_board)
+
+        if place_before == "c":
+            coldhot.boss()
+
         if hp <= 0:
             break
 
         elif place_before != "O":
             board[old_player_y][old_player_x] = place_before
+
 main()
