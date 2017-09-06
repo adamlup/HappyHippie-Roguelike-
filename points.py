9b6df7a832f0ccd9e58bb7cce19e0f52fdc4ec43
adamlup <adamlupi@gmail.com>
2017-09-06 15:03:58 +0200

    adding scores

---
+++ points.py
@@ -0,0 +1,13 @@
+import time
+
+def start_game():
+    start = time.time
+    return start
+
+def end_game():
+    end = time.time
+    return end
+
+def score(score):
+    score += 1
+    return score
