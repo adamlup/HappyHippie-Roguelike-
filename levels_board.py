5670f852e200fb2c16d553ed2cb01128e8eb6947
Makiwe <magdalenakielkowicz@gmail.com>
2017-09-06 11:21:16 +0200

    new board

--- levels_board.py
+++ levels_board.py
@@ -1,3 +1,18 @@
+def level_first_description():
+a
+    level_first_descyption = """ 
+    ##########################################################################################
+   #The first board is your Hippie working space.                                              #
+   #If he wont to leave this place he has to talk to all his "0-look-like" co-workers.        #
+   #Ther arguments are not the best and they can decrease your level of happiness.            #
+   #Just be patient, if you give them the time they will become bright stars in the company.  #
+   #This experience will be quite helpful when you met your boss in next level.               #
+   #                                                                                          #
+   #Good luck!                                                                                #
+   ########################################################################################## #
+   """
+   return level_first_descyption
+
 def level_one_board():
     level_one_board = '''
     ##############################################################
@@ -14,10 +29,60 @@ def level_one_board():
     #                                |     O|    |            |  #
     #                |            ___,    __,    |         ___,  #
     #~~   ~~~~~~~~~~~~               |      |    |          O |  #
-    #                |                            ~~~~~~   ~~~~~~# 
+    #                |                            ~~~~~~   ~~~~~~#
     #                |                                 |         #
     #                |___________ _________________    |         #
     #                            |                     |         #
     ##############################################################
     '''
     return level_one_board
+
+
+def level_second_description():
+    level_second_description = """
+    #############################################################################
+   # In this level, you Hippie will meet The Boss.                              #
+   # Before you get to his office you have to walk through the salty            #
+   # lake of tear those who were there before. Try not to touch them.           #
+   #                                                                            #
+   # When Hippie will finally meet The Boss, he will give you the final try.    #
+   # If you are smart you will defeat him.                                      #
+   # Yor new life is just right a corner! Stay  cautious and good luck!         #
+   #                                                                            #
+   ##############################################################################
+   """
+
+return level_second_descyption
+
+    def level_second_board():
+        level_second_board = '''
+        ####################
+        #                  #
+        # ~~         ~~    #
+         #     ~~           #
+          # ~~               #
+           #           ~~     #
+            # ~~               #
+             #          ~~~~~~~  ##############################################
+             #                                                                #
+             #~~~~~                                                           #
+             #          ~~~~~~~~~~~~~~~~~~~         @@@@@@@@@@@@@@@@@         #
+             #                                   @@@@@@@@@@@@@@@@@@@@@@@      #
+             #                                  @@@@@@@@@@@@@@@@@@@@@@@@@@    #
+             #~~~~~~~~~~~~~~~~                @@@@@@@@@@  @@@@@@@@@@@@@@@@@   #
+             #                                @@@@@@@@@    @@@@@@@@@@@@@@@@   #
+             #             ~~~~~~~~~~~~~~~~       @@@@@@  @@@@@@@@@@@@@@@@@   #
+             #                                        @@@@@@@@@@@@@@@@@@@@@   #
+             #                                          @@@@@@@@@@@@@@@@@@@   #
+             #                                            @@@@@@@@@@@@@@@@@   #
+             #                                          @@@@@@@@@@@@@@@@@@@   #
+             #                                      @@@@@@@@@@@@@@@@@@@@@@@   #
+             #                                    @@@@@@@@@@@@@@@@@@@@@@@@@   #
+             #                                @@@@@@@@@@@@@@@@@@@@@@@@@@@     #
+             #                                @@@@@@@@@@@@@@@@@@@@@@@@@@      #
+             #                                     @@@@@@@@@@@@@@@@@@@@       #
+             #                                        @@@@@@@@@@@@@@@@        #
+             #                                                                #
+             #                                                                #
+             #                                                                #
+             ##################################################################
