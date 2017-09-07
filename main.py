5d62104b77d51efbf5c7a6ca4a1254cf2a461190
adamlup <adamlupi@gmail.com>
2017-09-08 00:16:28 +0200

    finale version of main

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import startscreen
-import end
 import levels_board
 import drawboard
 import getch
-import os
 import hero_create
-import stats
-import time
-import points
-import hall_of_fame
+import os
+import thehangman
+import sys
+import end
 import coldhot
+import hall_of_fame
+import time
+
 
 def main():
-    #startscreen.print_start()
+    startscreen.print_start()
     level_one_board = drawboard.draw_board()
     board = drawboard.create_board(level_one_board)
 
     player_x = 5
     player_y = 2
-    
-    hp, attack = hero_create.hero_create()
-    live, atc = stats.enemy_stats(30, 15)
+    name = input("Enter your HappyHippie name: ")
+    happiness_level, convincing_power = hero_create.hero_create()
+    live = 20
+    atc = 10
     score = 0
-    name = input("Enter your name: ")
-    start = points.start_game()
+    exit_hangman = 0
+    levels_board.printing_level_first_description()
+    start = time.time()
     while True:
-        print("\tHero life = ", hp, "\t","\t","\t","\tscore = ", score, "\n\tHero attack = ", attack, end='')
+        print("\t Happines level = ", happiness_level, "\t", "\t", "\t", "\tchance to win = ",
+              score, "\n\tPower of arguments = ", convincing_power, end='')
         place_before = board[player_y][player_x]
         board = drawboard.insert_player(board, player_x, player_y)
         drawboard.print_board(board)
@@ -34,61 +38,71 @@ def main():
         old_player_y = player_y
         key = getch.getch()
         os.system("clear")
-        
+
         if key == 'w':
             player_y -= 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',' or board[player_y][player_x] == '"':
                 player_y += 1
 
         elif key == 'a':
             player_x -= 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',' or board[player_y][player_x] == '"':
                 player_x += 1
 
         elif key == 's':
             player_y += 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',' or board[player_y][player_x] == '"':
                 player_y -= 1
-            
+
         elif key == 'd':
             player_x += 1
-            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or  board[player_y][player_x] == '~' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',':
+            if board[player_y][player_x] == '#' or board[player_y][player_x] == '|' or board[player_y][player_x] == '_' or board[player_y][player_x] == ',' or board[player_y][player_x] == '"':
                 player_x -= 1
 
         elif key == 'q':
-            end = points.end_game()
             hall_of_fame.write_to_file(name, score)
-            break
-        
-        elif place_before == "p":
-            print("You find health potion")
-            hp += 45
+            sys.exit(0)
+
+        if place_before == "$":
+            print("You get a paycheck" + "\nYour level of happiness increased")
+            happiness_level += 100
             place_before = " "
-            
+
         if place_before == "O":
-            print("Player life {} attack enemy {}".format(hp, attack))
-            hp -= atc
-            print("Enemy life {} attack player {}".format(live, atc))
-            live -= attack
+            print("Player happines level {} attack co-worker {}".format(happiness_level, convincing_power))
+            happiness_level -= atc
+            print("Co-worker sadness {} attack player {}".format(live, atc))
+            live -= convincing_power
             if live <= 0:
                 board[old_player_y][old_player_x] = '*'
-                points.score(score)
-                score = points.score(score)
-                live = 30
+                score += 1
+                live = 20
             elif live > 0:
                 board[old_player_y][old_player_x] = place_before
-        
-        if score == 1 and place_before == "B":
+
+        if score > 0 and place_before == "B" and exit_hangman == 0:
+            board[old_player_y][old_player_x] = ' '
+            thehangman.game()
             level_second_board = drawboard.draw_second_board()
             board = drawboard.create_second_board(level_second_board)
+            levels_board.printing_level_second_description()
+            exit_hangman = 1
 
-        if place_before == "c":
-            coldhot.boss()
+        if place_before == "~":
+            print("")
+            happiness_level -= 30
 
-        if hp <= 0:
-            break
+        if place_before == ">":
+            coldhot.boss(score)
+            game_time = int(time.time() - start)
+            hall_of_fame.write_to_file(name, game_time)
+            end.win_screen()
+
+        if happiness_level <= 0:
+            end.lose_screen()
 
         elif place_before != "O":
             board[old_player_y][old_player_x] = place_before
+
 
 main()
