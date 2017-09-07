ea6cde3eec74fc67aaff859500ee02515cea123d
adamlup <adamlupi@gmail.com>
2017-09-08 00:13:46 +0200

    writing player achievement to file

--- hall_of_fame.py
+++ hall_of_fame.py
@@ -2,4 +2,3 @@ def write_to_file(name, time):
     with open("hall_of_fame.txt", "a", newline="", encoding='utf-8') as f:
         f.write('\nPlayer name: ' + str(name) + ',')
         f.write(' Time = ' + str(time) + ' seconds')
-        
