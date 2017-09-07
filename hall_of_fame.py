9b48473a210ba665a7f0cf3783e95512a45f3916
adamlup <adamlupi@gmail.com>
2017-09-08 00:02:25 +0200

    writing player name and time spent to file

--- hall_of_fame.py
+++ hall_of_fame.py
@@ -1,5 +1,5 @@
-def write_to_file(name, score):
-    with open("hall_of_fejm.txt", "a", newline="", encoding='utf-8') as f:
+def write_to_file(name, time):
+    with open("hall_of_fame.txt", "a", newline="", encoding='utf-8') as f:
         f.write('\nPlayer name: ' + str(name) + ',')
-        f.write(' Score = ' + str(score))
+        f.write(' Time = ' + str(time) + ' seconds')
         
