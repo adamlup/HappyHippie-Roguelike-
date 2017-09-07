3f44d07418d976a6cc2889195660d37e3b2e96a4
adamlup <adamlupi@gmail.com>
2017-09-08 00:00:42 +0200

    working boss mini game

--- coldhot.py
+++ coldhot.py
@@ -1,4 +1,7 @@
 import random
+import sys
+import end
+
 
 def get_random_number():
     number_list = []
@@ -6,60 +9,52 @@ def get_random_number():
         digit = random.randint(0, 9)
         if digit not in number_list:
             number_list.append(digit)
-    print(number_list)
     return number_list        
 
 
 def get_user_input():
    while True:
-    user_guess = input("Enter the number: ")
+    user_guess = input("Try to guess 3 digits number: ")
 
     if user_guess.isalpha():
         print("Enter only digits")
 
     elif len(user_guess) != 3:
-        print("Input hasnt 3 digits")
+        print("Your guess hasnt 3 digits")
 
     else:
         return list(user_guess)
 
 
 def compare_user_input_witth_answer(user_guess, correct_answer):
-    index = 0
     hint_list = []
-
+    index = 0
     for a in correct_answer:
         if str(a) == user_guess[index]:
-            hint_list.insert(0, "HOT")
-            
+           hint_list.insert(0, "HOT")
+           
         elif str(a) in user_guess:
-            hint_list.append("WORM")    
+           hint_list.append("WORM")    
         index += 1
 
-        if not hint_list:
-            hint_list.append("cold")
+    if not hint_list:
+        hint_list.append("cold")
 
-        return hint_list
+    return hint_list
+   
 
-
-def check_result(hint_list):
-    if hint_list == ["HOT"] * 3:
-        return True    
-
-
-def boss():
+def boss(score):
     correct_answer = get_random_number()
-
-    tries_left = 10
-    while tries_left > 0:
+    while score != 0:
+        print("\nChances to guess = ", score)
         user_guess = get_user_input()
-        result = compare_user_input_witth_answer(user_guess, correct_answer)
-        print(result)
-        if check_result(result):
-            print("WIN")
-            break
-        tries_left -= 1
-    if tries_left == 0:
-        print("LOSE")    
-
+        hint_list = compare_user_input_witth_answer(user_guess, correct_answer)
+        print(hint_list)
+        if hint_list == ['HOT'] * 3:
+            return     
+        else:
+            score -= 1
+    if score == 0:
+        end.lose_screen()
+        
 
