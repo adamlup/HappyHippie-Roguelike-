367ebaaa7b950c2056649435a6544d5d24f4ef30
adamlup <adamlupi@gmail.com>
2017-09-08 00:14:45 +0200

    choosing hero

--- hero_create.py
+++ hero_create.py
@@ -1,15 +1,15 @@
 def hero_create():
-    #name = input("Enter your name: ")
-    hero_choice = input("Chose your hero, knight(hp = 100, attack = 20) tank(hp = 140, attack = 9: ")
+    print("Chose your hero, handsome(happiness level = 70, convincing power = 7) inteligent(happiness level = 30, convincing power = 10): ")
+    hero_choice = input('\n To chose handsome type h, inteligent type i: ')
     while True:
-        if hero_choice == "knight":
-            hp = 100
-            attack = 20
+        if hero_choice == "h":
+            happiness_level = 70
+            convincing_power = 7
             break
-        elif hero_choice == "tank":
-            hp = 140
-            attack = 9
+        elif hero_choice == "i":
+            happiness_level = 30
+            convincing_power = 10
             break
         else:
-            hero_choice = input("Invalid choice(knight or tank): ")
-    return hp, attack
+            hero_choice = input("Invalid choice(h or i): ")
+    return happiness_level, convincing_power
