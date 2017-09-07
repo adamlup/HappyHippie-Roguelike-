1557f68cf36db20d1ef10cb4148973eeeca7ef63
adamlup <adamlupi@gmail.com>
2017-09-08 00:11:26 +0200

    printing level boards

--- drawboard.py
+++ drawboard.py
@@ -1,8 +1,10 @@
 import levels_board
 
+
 def draw_board():
     level_one_board = levels_board.level_one_board()
     return level_one_board
+
 
 def create_board(level_one_board):
     level_one_board = level_one_board.split("\n")
@@ -12,9 +14,11 @@ def create_board(level_one_board):
         board.append(list(element))
     return board
 
+
 def draw_second_board():
-    level_second_board = levels_board.level_second_board() 
+    level_second_board = levels_board.level_second_board()
     return level_second_board
+
 
 def create_second_board(level_second_board):
     level_second_board = level_second_board.split('\n')
@@ -23,12 +27,14 @@ def create_second_board(level_second_board):
     for element in level_second_board:
         board.append(list(element))
     return board
-    
+
+
 def print_board(board):
     for row in board:
         for char in row:
             print(char, end='')
         print()
+
 
 def insert_player(board, width, height):
     board[height][width] = '@'
